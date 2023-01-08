b4dc5202d52f5a75b71dadea0d1c8890e8bfc820
PRATIK-2251 <pratikhole22@gmail.com>
2023-01-08 11:57:16 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import {
+  Container,
+  Box,
+  Paper,
+  Button,
+  Stack,
+  Typography,
+} from "@mui/material";
+const App = () => {
+  const [advice, setAdvice] = useState("");
+  useEffect(() => {
+    giveAdvice();
+  }, []);
+  async function giveAdvice() {
+    await axios("https://api.adviceslip.com/advice").then((res) => {
+      console.log(res.data.slip.advice);
+      setAdvice(res.data.slip.advice);
+    });
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <Container
+        maxWidth={false}
+        sx={{ backgroundColor: "#ffe082", minHeight: "90vh" }}
+      >
+        <Box sx={{ width: "100%", marginBottom: 3, mt: 3 }}>
+          <Stack spacing={2} marginTop={5}>
+            <Typography sx={{ textAlign: "center" }}>
+              Random Quote Generator
+            </Typography>
+          </Stack>
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            "& > :not(style)": {
+              m: 1,
+              width: "auto",
+              height: 128,
+              minWidth: "30rem",
+            },
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Paper elevation={4} sx={{ display: "grid" }}>
+            <Typography
+              sx={{ textAlign: "center", marginY: 2, paddingX: "10px  " }}
+            >
+              {advice}
+            </Typography>
+            <Stack sx={{ display: "flex", alignItems: "center" }}>
+              <Button
+                variant="contained"
+                sx={{
+                  width: "auto",
+                  padding: 1,
+                  paddingX: 1,
+                }}
+                onClick={() => giveAdvice()}
+              >
+                Give Me Advice
+              </Button>
+            </Stack>
+          </Paper>
+        </Box>
+      </Container>
+    </>
   );
-}
+};
 
 export default App;
